cb7359189c50b9f13212118ea9dd9fa0d96bb684
Katrina.Tomaszczyk <katrina.tomaszczyk@ladbrokes.com.au>
2024-06-12 13:34:50 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# vue3-quiz
+
+Practice vue 3 by building a Quiz App.
+
+Course is from Udemy by instructor: Laith Harb
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+## Screenshots
+
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search..." />
+    </header>
+  </div>
+  <div class="options-container">
+    <div class="card">
+      <img src="/src/assets/images/Math.jpeg" alt="" />
+      <div class="card-text">
+        <h2>Math</h2>
+        <p>15 questions</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scroped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
