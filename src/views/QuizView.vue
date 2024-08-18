67df29ad4b5aed5abcb9dfb9819e7fec730888c3
Katrina.Tomaszczyk <katrina.tomaszczyk@ladbrokes.com.au>
2024-08-19 09:14:52 +1000

    feat: pass quiz props

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,94 +1,24 @@
 <template>
   <div>
-    <header>
-      <h4>Question</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader />
     <div>
-      <div class="question-container">
-        <h1 class="question"></h1>
-      </div>
-      <div class="option-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p></p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">B</p>
-          <div class="option-value">
-            <p></p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">C</p>
-          <div class="option-value">
-            <p></p>
-          </div>
-        </div>
-        <div class="option">
-          <p class="option-label">D</p>
-          <div class="option-value">
-            <p></p>
-          </div>
-        </div>
-      </div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
   </div>
 </template>
 
-<style scoped>
-header {
-  margin-top: 20px;
-}
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+import quizes from "../data/quizes.json";
 
-header h4 {
-  font-size: 30px;
-}
+const route = useRoute();
 
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid bisque;
-}
+const quizId = parseInt(route.params.id);
 
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: bisque;
-}
+const quiz = quizes.find((q) => q.id === quizId);
 
-.question-container {
-  margin-top: 20px;
-}
-
-.question {
-  font-size: 40px;
-  margin-bottom: 20px;
-}
-
-.option {
-  display: flex;
-  margin-bottom: 20px;
-}
-
-.option-label {
-  background-color: bisque;
-  width: 50px;
-  height: 50px;
-  font-size: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.option-value {
-  background-color: rgb(244, 239, 239);
-  width: 100%;
-  font-size: 30px;
-  padding: 0 20px;
-}
-</style>
+const currentQuestionIndex = ref(0);
+</script>
