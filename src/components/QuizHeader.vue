41ef63eb920e47933ad5c15e356361ccb9659774
Katrina.Tomaszczyk <katrina.tomaszczyk@ladbrokes.com.au>
2024-08-26 10:35:09 +1000

    feat: computed values for barPercentage

--- src/components/QuizHeader.vue
+++ src/components/QuizHeader.vue
@@ -1,11 +1,20 @@
 <template>
   <header>
-    <h4>Question</h4>
+    <h4>Question {{ questionStatus }}</h4>
     <div class="bar">
-      <div class="completion"></div>
+      <div class="completion" :style="{ width: barPercentage }"></div>
     </div>
   </header>
 </template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const { questionStatus, barPercentage } = defineProps([
+  "questionStatus",
+  "barPercentage",
+]);
+</script>
 
 <style scoped>
 header {
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <QuizHeader />
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
 
@@ -11,14 +15,25 @@
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "../data/quizes.json";
 
 const route = useRoute();
-
 const quizId = parseInt(route.params.id);
-
 const quiz = quizes.find((q) => q.id === quizId);
-
 const currentQuestionIndex = ref(0);
+
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`);
+
+// watch(() => currentQuestionIndex.value, () => {
+//   questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+// })
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
 </script>
